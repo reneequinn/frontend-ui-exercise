ce2b65be2814e104895b4a7345f490b43d7298ea
Renee Quinn <reneequinn17@gmail.com>
2020-09-25 14:12:13 +0930

    Adds hero section, with styling and functionality

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,6 @@
+import Glide from '@glidejs/glide';
+
+// Nav
 const button = document.querySelector('.menu-btn');
 const nav = document.querySelector('nav');
 
@@ -5,3 +8,13 @@ button.addEventListener('pointerdown', (e) => {
   nav.classList.toggle('open');
   e.currentTarget.classList.toggle('open');
 });
+
+// Hero
+const heroGlide = new Glide('.hero', {
+  type: 'carousel',
+  autoplay: 5000,
+  animationDuration: 700,
+  gap: 0,
+});
+
+heroGlide.mount();
